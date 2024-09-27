b00c63773973939f962077565445ba2766c5e2a5
EV4T <eduardo.m.pereira.prestserv@petrobras.com.br>
2024-09-27 12:50:35 -0300

    Add project files.

---
+++ Skinet_API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet_API.Entities;
+using Skinet_Infrastructure.Data;
+
+namespace Skinet_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Getproducts() {
+            var products = await _storeContext.Products.ToListAsync() ;
+            return Ok(products);
+
+        }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Product>> GetProductById(int Id)
+        {
+            var userById = await _storeContext.Products.FindAsync(Id);
+            return Ok(userById);
+        }
+    }
+}
---
+++ Skinet_Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skinet_Core.Entities;
+namespace Skinet_Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
